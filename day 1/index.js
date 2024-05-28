ce2dff24c10083d21c086fd181abd917cf764315
ruth-chirwa <chirwaruth647@gmail.com>
2024-05-28 11:08:27 -0400

    javascript

---
+++ day 1/index.js
@@ -0,0 +1,14 @@
+//let keyword - for reasiggning array values
+let rows = ["ruth", "eunice", "amon"];
+rows[1] = 25;
+console.log(rows);
+
+//reasigning values - string values
+let one = "ruth";
+one = "eunice";
+console.log(one);
+
+//reassigning variables
+let two = "amon";
+two = one;
+console.log(two);
